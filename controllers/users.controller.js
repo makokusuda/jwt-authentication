b1549b450c5d39e48c32452707f9f4b8f920b405
Mako Kusuda <mako.kusuda@gmail.com>
2021-06-19 16:31:50 +0900

    Add sign in

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+
+const helpers = require("../helpers/helpers");
 const { User, RefreshToken } = require("../models/index");
 
 const getAllUsers = async (req, res) => {
@@ -16,11 +18,47 @@ const createUser = async (req, res) => {
   try {
     await User.create({ userName, password: hashedPassword });
     return res
-      .status(200)
+      .status(201)
       .send({ message: "User was registered successfully!" });
   } catch (err) {
-    return res.status(500).send({ message: err });
+    return res.status(400).send({ message: err });
   }
 };
 
-module.exports = { getAllUsers, createUser };
+const signIn = async (req, res) => {
+  const { userName, password } = req.body;
+  try {
+    const user = await User.findAll({
+      where: {
+        userName,
+      },
+    });
+
+    if (user.length === 0) {
+      return res.status(401).send({ message: "User is not found" });
+    }
+
+    const passwordIsValid = await bcrypt.compareSync(
+      password,
+      user[0].password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!",
+      });
+    } else {
+      const accessToken = helpers.generateAccessToken(userName);
+      const refreshToken = await helpers.generateRefreshToken();
+      return res.status(200).send({
+        accessToken,
+        refreshToken,
+      });
+    }
+  } catch (err) {
+    return res.status(404).send({ message: err });
+  }
+};
+
+module.exports = { getAllUsers, createUser, signIn };
