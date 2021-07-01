447ea29741b5ba4a6b6090772fdb5bb4cc750e35
Mako Kusuda <mako.kusuda@gmail.com>
2021-07-01 13:34:46 +0900

    Setup for heroku

--- models/config.js
+++ models/config.js
@@ -4,7 +4,16 @@ const { Sequelize } = require("sequelize");
 let db;
 
 if (process.env.NODE_ENV === "production") {
-  db = new Sequelize(process.env.DATABASE_URL);
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   db = new Sequelize("test_db", process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "localhost",
