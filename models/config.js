1d6aaebd51454f12ad263962263aafaf0cfa47e1
Mako Kusuda <mako.kusuda@gmail.com>
2021-07-01 12:39:52 +0900

    Setup for heroku

--- models/config.js
+++ models/config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const pg = require("pg");
 const { Sequelize } = require("sequelize");
+let db;
 
-const db = new Sequelize(
-  "test_db",
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+if (process.env.NODE_ENV === "production") {
+  db = new Sequelize(process.env.DATABASE_UR);
+} else {
+  db = new Sequelize("test_db", process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "localhost",
     dialect: "postgres",
-  }
-);
+  });
+}
 
 module.exports = db;
